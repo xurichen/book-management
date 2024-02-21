60a3ce8b4e45118818fe44fb62b81e2b3f869746
Xuri <chenxuri@gmail.com>
2024-02-22 00:43:57 +0800

    book-managment-005 add missing files

--- management/src/main/java/com/book/management/controller/BookController.java
+++ management/src/main/java/com/book/management/controller/BookController.java
@@ -1,9 +1,13 @@
 package com.book.management.controller;
 
 import com.book.management.entity.Book;
-import org.springframework.stereotype.Controller;
+import com.book.management.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,10 +18,55 @@ public class BookController {
     public String getHome(){
         return "Home";
     }
+
+    @Autowired
+    private BookService bookService;
     @GetMapping("/books")
-    public List<Book> getBooks(){
-        var books = new ArrayList<Book>();
-        books.add(new Book(1,"Yellow","A","2024","ISBN-111"));
-        return books;
+    public ResponseEntity<List<Book>> getAllBooks() {
+        List<Book> books = bookService.getAllBook();
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+    @PostMapping("/books")
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        bookService.save(book);
+        return new ResponseEntity<>(book, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/books/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable int id) {
+        Book book = bookService.getBookById(id);
+        if (book != null) {
+            return new ResponseEntity<>(book, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/books/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable int id, @RequestBody Book book) {
+        Book existingBook = bookService.getBookById(id);
+        if (existingBook != null) {
+            // Update the existing book with the details from the provided book object
+            existingBook.setTitle(book.getTitle());
+            existingBook.setAuthor(book.getAuthor());
+            existingBook.setIsbn(book.getIsbn());
+            existingBook.setPublishedYear(book.getPublishedYear());
+            bookService.save(existingBook);
+            return new ResponseEntity<>(existingBook, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/books/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable int id) {
+        Book book = bookService.getBookById(id);
+        if (book != null) {
+            bookService.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
--- management/src/main/java/com/book/management/entity/Book.java
+++ management/src/main/java/com/book/management/entity/Book.java
@@ -1,2 +1,26 @@
-package com.book.management.entity;public class Book {
+package com.book.management.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String author;
+    private String publishedYear;
+    private String isbn;
+
+
 }
